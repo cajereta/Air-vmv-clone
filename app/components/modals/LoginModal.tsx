b1f28d43e76004d4e4ad4506fe817890b48e07ac
Nespera <msijuicebox@gmail.com>
2023-05-09 01:12:29 -0300

    first commit

---
+++ app/components/modals/LoginModal.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
+import useLoginModal from "@/app/hooks/useLoginModal";
+
+const LoginModal = () => {
+  const router = useRouter();
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors,
+    }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    signIn("credentials", {
+      ...data,
+      redirect: false
+    })
+      .then((callback) => {
+        setIsLoading(false);
+
+        if (callback?.ok) {
+          toast.success("Logged in");
+          router.refresh();
+          loginModal.onClose();
+        }
+
+        if (callback?.error) {
+          toast.error(callback.error);
+        }
+      });
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome back"
+        subtitle="Login to your account"
+
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => { }}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light ">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>
+            First time using Airbnb?
+          </div>
+          <div
+            onClick={toggle}
+            className="text-neutral-800 cursor-pointer hover:underline">
+            Create an account
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Log in"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default LoginModal;
---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import './globals.css';
+import ClientOnly from './components/ClientOnly';
+import Navbar from './components/navbar/Navbar';
+import { Inter } from 'next/font/google';
+import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Tutorial hell',
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
